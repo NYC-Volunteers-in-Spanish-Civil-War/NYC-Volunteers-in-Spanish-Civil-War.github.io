4c3fd93a2870151e11d6cb7f8ab81dddfba4bdcf
Ian Williams <norbekian9@gmail.com>
2020-06-30 14:24:01 -0400

    Added checksums.

--- temp.py
+++ temp.py
@@ -12,8 +12,13 @@ for key in json_data:
     master_data = {}
     for k in master_keys:
         master_data[k] = data[k]
-    master_json[md5(key).hexdigest()] = master_data
     with open('archive/data/' + md5(key).hexdigest() + '.json', 'w+') as s:
         json.dump(data, s, indent=4, sort_keys=True)
+    hash_md5 = md5()
+    with open('archive/data/' + md5(key).hexdigest() + '.json', "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    master_data["checksum"] = hash_md5.hexdigest()
+    master_json[md5(key).hexdigest()] = master_data        
 with open('archive/data/master.json', 'w+') as f:
         json.dump(master_json, f, indent=4, sort_keys=True)
