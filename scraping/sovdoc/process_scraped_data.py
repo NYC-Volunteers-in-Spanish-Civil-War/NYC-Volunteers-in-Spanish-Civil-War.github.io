778db3fad2fc9c08b2f9d4febe60866f29e98993
Theadora Williams <theadorabledeveloper@gmail.com>
2021-05-01 17:30:12 -0400

    Cleaned COMINTERN document data representation. Input is now properly sanitized and sorted

--- scraping/sovdoc/process_scraped_data.py
+++ scraping/sovdoc/process_scraped_data.py
@@ -11,6 +11,8 @@ python3 process_scraped_data.py
 import re
 import json
 from pprint import pprint
+from urllib.parse import quote_plus
+from html import unescape
 
 # Each file/folder in the dataset
 MASTER_DATA = {}
@@ -23,6 +25,9 @@ interbrigades_data = json.loads(open('interbrigades_data.json', 'r').read())
 # A function to turn various iterations of the directory locations into a single consistent one with only digits seperated by periods.
 standardize = lambda text: "_".join(re.findall("\w*(?<!_)\d+", text))
 
+# Helper function for sorting documents by their id
+sort_documents = lambda items: sorted(items, key = lambda k: [int(i) for i in k.split('_')])
+
 # Loop over sovdoc data and clean, add to master dataset
 for item in sovdoc_data:
     id = standardize(sovdoc_data[item]['code'])
@@ -34,7 +39,7 @@ for item in sovdoc_data:
 # Change child references to use the standardized id and adding child count
 for item in sorted(list(MASTER_DATA.keys())):
     if 'children' in MASTER_DATA[item]:
-        MASTER_DATA[item]['children'] = [sovdoc_data[child]['id'] for child in MASTER_DATA[item]['children']]
+        MASTER_DATA[item]['children'] = sort_documents([sovdoc_data[child]['id'] for child in MASTER_DATA[item]['children']])
         MASTER_DATA[item]['child_count'] = len(MASTER_DATA[item]['children'])
 
 # Further flatten/condense image data and adding child count
@@ -44,7 +49,11 @@ for item in MASTER_DATA:
         for img_set in MASTER_DATA[item]['images']]
     if MASTER_DATA[item]['images'] and not MASTER_DATA[item]['child_count']:
         MASTER_DATA[item]['child_count'] = sum([int(i[2]) - int(i[1]) for i in MASTER_DATA[item]['images']])
-               
+
+
+
+# Helper function for dealing with encoding
+clean_name = lambda k : unescape(k).strip('.').replace('/', '*').replace('.html','')
 # Integrate metadata from the interbrigades files into masterdata and tags dict
 for item in interbrigades_data:
     id = standardize(item['id'])
@@ -53,16 +62,20 @@ for item in interbrigades_data:
     del item['node_number']
     del item['id']
     # Remove trailing periods
-    MASTER_DATA[id]['meta'] = {k.strip('.'): [v.strip('.') for v in item[k]] for k in item}
+    MASTER_DATA[id]['meta'] = {clean_name(k): [clean_name(v) for v in item[k]] for k in item}
     for field in MASTER_DATA[id]['meta']:
-        META_TAGS.setDefault(field, {})
+        print(field)
+        META_TAGS.setdefault(field, {})
         for tag in MASTER_DATA[id]['meta'][field]:
-            META_TAGS[field].setDefault(tag, {}).append(id)
+            META_TAGS[field].setdefault(tag, []).append(id)
 
 
     pprint(MASTER_DATA[id])
 
-META_TAGS = { **{'tags': list(META_TAGS.keys())}, **{field:list(META_TAGS[field].keys()) for field in META_TAGS}, **{field + '_'  + tag:META_TAGS[field][tag] for field in META_TAGS for tag in META_TAGS[field]}}
+
+META_TAGS = { **{'tags': list(META_TAGS.keys())},
+              **{field:list(META_TAGS[field].keys()) for field in META_TAGS},
+              **{tag:sort_documents(META_TAGS[field][tag]) for field in META_TAGS for tag in META_TAGS[field]}}
 
 json.dump(MASTER_DATA, open('documents.json', 'w+'))
 json.dump(META_TAGS, open('meta_tags.json', 'w+'))
