54a34df9e43a12188bec82749699090e4df15880
Theadora Williams <theadorabledeveloper@gmail.com>
2021-07-13 18:45:48 -0400

    The editor option is no longer shown unless the site is running locally/you actually are an editor.

--- routes/__init__.py
+++ routes/__init__.py
@@ -7,6 +7,7 @@ import subprocess
 import json
 import urllib
 import requests
+import datetime
 import jinja2
 from flask_ckeditor import CKEditor, upload_success, upload_fail
 from hashlib import md5
@@ -18,16 +19,34 @@ from flask import Flask, Blueprint, request, redirect, url_for, render_template,
 from flask import Blueprint, Flask
 routes = Blueprint("routes", __name__)
 
+CONF = {"debug": True}
+
 freezer = Freezer(None, with_no_argument_rules=False, log_url_for=False)
 
 volunteer_freezer = Freezer(None, with_no_argument_rules=False, log_url_for=False)
 
 misc_freezer = Freezer(None, with_no_argument_rules=False, log_url_for=False)
 
+MASTER_FILE = 'archive/data/master.json'
 
+def get_data_from_file(filename, url=False):
+    """ Returns the json data in a given file. """
+    if not url:
+        with open(filename, 'r') as f:
+            return json.load(f)
+    else:
+        response = urllib.request.urlopen(filename)
+        return json.loads(response.read())
+def write_data_to_file(filename, data):
+    """ Writes data to a json file. """
+    with open(filename, 'w+') as f:
+        json.dump(data, f, indent=4, sort_keys=True)
+
+
+from .misc import *
+from .build_hooks import *
 from .editor import *
 from .volunteers import *
-from .build_hooks import *
 from .documents import *
-from .misc import *
+
 
